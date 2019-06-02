8a8982d7cb4c0001ddb27ef08b3f8290602b6188
Brasoveanu Gheorghita <51229567+BrasoveanuGhe@users.noreply.github.com>
2019-06-02 21:05:49 +0300

    Update avr_extInterrupt.h

--- InternshipProject/avr_extInterrupt.h
+++ InternshipProject/avr_extInterrupt.h
@@ -26,17 +26,17 @@ ExternalBits interrupt_init;
 
 
 /**	
- * \brief							configures the external interrupt control register
+ * \brief					configures the external interrupt control register
  * 
  * \param interrupt_Reg				register A (EICRA) or register B (EICRB)
  * \param interrupt_Pin				select the pin on which the interruption occurs
- *									bits 7:0 – INTn: [n = 7:0]
+ *									bits 7:0 â€“ INTn: [n = 7:0]
  *
- * \param interrupt_Control			bits 7:0 – INTn: [n = 7:0]
- *									LOW_LVL_n -> The low level of INTn generates an interrupt request
- * 									RESERVED_n -> Reserved.
- *									FALLING_EDGE_n -> The falling edge of INTn generates asynchronously an interrupt request.
- *									RISING_EDGE_n -> The rising edge of INTn generates asynchronously an interrupt request.
+ * \param interrupt_Control			bits 7:0 â€“ INTn: [n = 7:0]
+ *						LOW_LVL_n -> The low level of INTn generates an interrupt request
+ * 						RESERVED_n -> Reserved.
+ *						FALLING_EDGE_n -> The falling edge of INTn generates asynchronously an interrupt request.
+ *						RISING_EDGE_n -> The rising edge of INTn generates asynchronously an interrupt request.
  *									
  * \return void
  */
@@ -45,10 +45,10 @@ void set_extInterrupt(volatile uint8_t *interrupt_Reg, uint8_t interrupt_Pin, ui
 
 
 /**
- * \brief						external interrupt request n Enable [n = 7:0]
+ * \brief				external interrupt request n Enable [n = 7:0]
  * 
  * \param interrupt_Reg			port address
- * \param mask					Bits 7:0 – INTn: External Interrupt Request n Enable [n = 7:0]
+ * \param mask				Bits 7:0 â€“ INTn: External Interrupt Request n Enable [n = 7:0]
  * 
  * \return void
  */
@@ -56,10 +56,10 @@ void set_extInterrupt_Enable(volatile uint8_t *interrupt_Reg, uint8_t mask);
 
 
 /**
- * \brief						external interrupt request n Disable [n = 7:0]
+ * \brief				external interrupt request n Disable [n = 7:0]
  * 
  * \param interrupt_Reg			port address
- * \param mask					Bits 7:0 – INTn: External Interrupt Request n Disable [n = 7:0]
+ * \param mask				Bits 7:0 â€“ INTn: External Interrupt Request n Disable [n = 7:0]
  * 
  * \return void
  */
